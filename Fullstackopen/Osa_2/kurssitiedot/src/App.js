62a2318227be1fd98d43c5814845a7d9aa99c390
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-10-31 13:42:01 +0200

    kurssitiedot 2.5

--- Fullstackopen/Osa_2/kurssitiedot/src/App.js
+++ Fullstackopen/Osa_2/kurssitiedot/src/App.js
@@ -1,40 +1,5 @@
-const Header = ({ course }) => <h1>{course}</h1>
+import Course from './Course'
 
-const Total = ({ sum }) => {
-  const result = sum.map(part => part.exercises).reduce((partialSum, a) => partialSum + a,0)
-return(
-  <p>
-    The total is {result}
-  </p>
-)
-}
-
-
-const Part = ({ part }) => {
-return(
-  <ul>
-    <li>
-      {part.name} {part.exercises}
-    </li>
-  </ul>
-)
-}
-const Content = ({ parts }) => 
-  <>
-  {parts.map(part => <Part key = {part.id} part ={part}/>)}
-
-  </>
-
-const Course = ({course}) =>{
-  console.log(course, "Course")
-  return(
-    <div>
-    <Header course = {course.name}/>  
-    <Content parts = {course.parts}/>
-    <Total sum = {course.parts}/>
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
