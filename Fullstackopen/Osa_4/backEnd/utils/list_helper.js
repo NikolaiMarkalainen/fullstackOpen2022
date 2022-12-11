72359dc00e5d208001e4b3f61b8bb88521723313
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-12-11 17:48:09 +0200

    blog 4.4

--- Fullstackopen/Osa_4/backEnd/utils/list_helper.js
+++ Fullstackopen/Osa_4/backEnd/utils/list_helper.js
@@ -4,6 +4,30 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+    if (blogs.length === 1){
+        const initialValue = 0
+        const val = blogs.reduce(
+            (accumulator,currentValue)=> accumulator + currentValue.likes,
+            initialValue
+        )
+            return val;
+    }
+    else if(blogs.length > 1){
+        const initialValue = 0
+        const totalSum = blogs.reduce(
+            (accumulator,currentValue) => accumulator + currentValue.likes,
+            initialValue
+        )
+        return totalSum;
+    }
+    else if(blogs === 0){
+        return 0
+    }
 }
+
+module.exports = {
+    dummy,
+    totalLikes
+}
+
