88cd59e04567e7d502098518d03b9b4a3cbe8e64
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-12-19 16:11:03 +0200

    blog 5.2

--- Fullstackopen/Osa_5/bloglist-frontend/src/App.js
+++ Fullstackopen/Osa_5/bloglist-frontend/src/App.js
@@ -17,6 +17,24 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+  
+  
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
   
@@ -24,6 +42,10 @@ const App = () => {
     const user = await loginService.login({
       username,password,
     })
+
+    window.localStorage.setItem(
+      'loggedBlogappUser', JSON.stringify(user)
+    )
     blogService.setToken(user.token)
     setUser(user)
     setUsername('')
@@ -34,6 +56,14 @@ const App = () => {
       setErrorMessage(null)
     }, 5000)
   }
+  }
+  
+
+  const handleLogout = async (event) =>{
+    event.preventDefault()
+    user.token = null
+    window.localStorage.clear()
+    window.location.reload()
   }
   
   const addBlog = () => {
@@ -80,12 +110,6 @@ const App = () => {
     </form>      
   )
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )  
-  }, [])
-
   return (
     <div>
      <h1>BLOGS</h1>
@@ -96,11 +120,12 @@ const App = () => {
       <p>{user.name} logged in</p>
       {blogForm()}
       {console.log("in logged in")}
-     </div>
-     }
+      <button type="submit" onClick={handleLogout}>log out</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+     </div>
+     }
     </div>
   )
 }
