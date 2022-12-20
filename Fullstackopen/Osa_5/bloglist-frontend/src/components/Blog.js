217a2fc7044037687aca24a533ff40ff07ef2aea
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2022-12-20 19:02:42 +0200

    blog 5.7

--- Fullstackopen/Osa_5/bloglist-frontend/src/components/Blog.js
+++ Fullstackopen/Osa_5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,21 @@
+import Togglable from "./Togglable"
+
+
 const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
+  <ul>
+    <li>
+      Title: {blog.title}
+    </li>
+<Togglable buttonLabel="Show Details">
+    <div>
+      Author: {blog.author}
+      <br></br>
+      Url: {blog.url}
+      <br></br>
+      <button>Like</button>: {blog.likes}
+    </div>
+</Togglable>
+  </ul>  
 )
 
 export default Blog
