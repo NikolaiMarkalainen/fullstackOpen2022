867d889d444d384ff97046028ed7cc7d77fcdbc6
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2023-01-07 17:32:29 +0200

    anecdote 6.13 && 6.14

--- Fullstackopen/Osa_6/redux-anecdotes/src/components/AnecdoteForm.js
+++ Fullstackopen/Osa_6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,22 +1,21 @@
 import { useDispatch, useSelector } from "react-redux"
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { removeNotification,addNotification } from "../reducers/notificationReducer"
+import anecdoteService from "../services/anecdotes"
 const NewAnecdote = (props) => {
     const dispatch = useDispatch()
-    const addAnecdote = useSelector(({anecdotes,notification}) => {
-        return event => {
+    const addAnecdote = async (event) => {
             event.preventDefault()
             const content = event.target.anecdote.value
             console.log(event.target.anecdote)
             event.target.anecdote.value = ''
-            dispatch(addNotification(content)) &&
-            dispatch(createAnecdote(content)) &&
+            const newAnecdote = await anecdoteService.createNew(content)
+            dispatch(createAnecdote(newAnecdote)) &&
+            dispatch(addNotification(newAnecdote)) &&
             setTimeout(() => {
                 dispatch(removeNotification(content))
             }, 5000)
         }
-    })
-    
     return(
         <div>   
             <h2>create new</h2>
