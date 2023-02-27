a88c1f6644380d8b5228bb5d07c80154ce33cc74
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2023-02-27 18:09:47 +0200

    updated 3.3

--- Fullstackopen/Osa_13/PostGreBlog/index.js
+++ Fullstackopen/Osa_13/PostGreBlog/index.js
@@ -1,17 +1,61 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+    
+    },
+});
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+class Blog extends Model {}
+    Blog.init({
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    url:{
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    author:{
+        type: DataTypes.TEXT
+    },
+    title:{
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    likes:{
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+    }, {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'blog'
+    })
 
-main()
+
+
+app.get('/api/blogs', async (req,res) => {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+})
+
+app.post('/api/blogs', async (req,res) => {
+    try{
+        console.log(req.body)
+        const blog = await Blog.create(req.body)
+        res.json(blog)
+    } catch(error){
+        return res.status(400).json({ error })
+    }
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running in port: ${PORT}`)
+})
