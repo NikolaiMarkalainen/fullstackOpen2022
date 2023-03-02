6f99452be2fbe5bbe42d9e17286bb450e182284e
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2023-03-02 22:07:23 +0200

    13.22

--- Fullstackopen/Osa_13/PostGreBlog/controllers/readinglistRouter.js
+++ Fullstackopen/Osa_13/PostGreBlog/controllers/readinglistRouter.js
@@ -13,4 +13,15 @@ router.post('/', async (req, res) => {
     else throw Error('No data')
 })
 
+router.put('/:id', async (req, res) => {
+    console.log(req.body)
+    console.log(req.params.id)
+    const readingList = await Readinglists.findByPk(req.params.id)
+    readingList.read = req.body.read
+    await readingList.save()
+    if(readingList) res.json(readingList)
+    else throw Error('No data')
+
+})
+
 module.exports = router
