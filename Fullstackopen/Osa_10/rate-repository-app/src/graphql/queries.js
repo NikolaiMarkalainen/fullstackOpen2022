ca2468b9f2a08aa507991edfda209f8bb7e21b0b
Nikolai Markalainen <nikolai.markalainen@gmail.com>
2024-02-24 09:58:06 +0200

    10.11 added GraphQL query to fetch data from API

---
+++ Fullstackopen/Osa_10/rate-repository-app/src/graphql/queries.js
@@ -0,0 +1,26 @@
+import { gql, useQuery } from "@apollo/client";
+
+export const GET_REPOSITORIES = gql `
+    query{
+        repositories {
+            edges {
+              node {
+                createdAt
+                description
+                forksCount
+                fullName
+                id
+                language
+                name
+                openIssuesCount
+                ownerAvatarUrl
+                ownerName
+                ratingAverage
+                reviewCount
+                stargazersCount
+                url
+                watchersCount
+              }
+            }
+          }
+    }`;
---
+++ Fullstackopen/Osa_10/rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,19 @@
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+import { useQuery } from '@apollo/client';
+const useRepositories = () => {
+ const {data, error, loading} = useQuery(GET_REPOSITORIES, {
+  fetchPolicy: 'cache-and-network'
+});
+  if (loading) {
+    return { loading: true, error: null, repositories: null };
+  }
+
+  if (error) {
+    return { loading: false, error, repositories: null };
+  }
+
+  return { loading: false, error: null, repositories: data.repositories };
+};
+
+export default useRepositories
