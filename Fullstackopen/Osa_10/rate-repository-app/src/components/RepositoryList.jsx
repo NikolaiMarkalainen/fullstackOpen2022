9cb6a0fec18b7af138ca80c6dd4d3ed27710a641
Nikolai Markalainen <nikolai.markalainen@gmail.com>
2024-02-26 21:16:44 +0200

    10.23 added filtration on data rendering

--- Fullstackopen/Osa_10/rate-repository-app/src/components/RepositoryList.jsx
+++ Fullstackopen/Osa_10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,15 +1,87 @@
-import { FlatList, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { FlatList, View, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
+import Text from './Text';
 import RepositoryItems from './RepositoryItems';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
+import { useEffect, useState } from 'react';
+import { Button, Menu, PaperProvider, Icon } from 'react-native-paper';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  menuProvider: {
+    
+  }
 });
 
 
+
+const HeaderFilter = ({setOrderBy, setOrderDirection, orderBy, orderDirection}) => {
+// ASC DESC
+
+// CREATED_AT RATING_AVERAGE
+
+console.log("PROPS", setOrderBy, setOrderDirection)
+const [visible, setVisible] = useState(false);
+const [text, setText] = useState("Latest Repositories")
+const openMenu = () => setVisible(true);
+
+const closeMenu = () => setVisible(false);
+
+
+
+  const handleFilterChange = (order) => {
+    if(order === 'ASC'){
+      setOrderDirection('ASC') 
+      setOrderBy('RATING_AVERAGE')
+    }
+    if(order === 'DESC'){
+      setOrderDirection('DESC')
+      setOrderBy('RATING_AVERAGE')
+    }
+    if(order === 'CREATED_AT'){
+      setOrderBy('CREATED_AT') 
+      setOrderDirection('ASC') 
+    }
+  };
+
+  useEffect(() => {
+    if(orderBy === 'RATING_AVERAGE'){
+      if (orderDirection === 'ASC') {
+        setText('Lowest Rated');
+      } else if (orderDirection === 'DESC') {
+        setText('Highest Rated');
+    }
+    } else if (orderBy === 'CREATED_AT') {
+      setText('Latest Repositories');
+    }
+  }, [orderDirection, setOrderBy, setOrderDirection]);
+  return (
+    <View style={styles.menuProvider}>
+      <Menu
+        visible={visible}
+        style={{}}
+        onDismiss={closeMenu}
+        anchor={
+          <TouchableOpacity onPress={openMenu} style={{flex:1, flexDirection:'row', padding: 20}}>
+                  <Button style={{ flex:1, justifyContent: 'center', alignItems: 'center'}} onPress={openMenu}> 
+                  <Text style={{ fontSize: 16 }}>{text}</Text> </Button>
+                  <View style={{ flex: 1,padding: 10, alignItems: 'flex-end'}}>
+                    <Icon onPress={openMenu} size={32} source="arrow-down-bold-box" color='white'/>
+                  </View>
+            </TouchableOpacity>
+        }>
+            <View style={{flex: 1, alignItems: 'flex-start'}}>
+              <Menu.Item  onPress={() => handleFilterChange('ASC')} title="Lowest Rated" />
+              <Menu.Item onPress={() => handleFilterChange('DESC')} title="Highest Rated" />
+              <Menu.Item onPress={() => handleFilterChange('CREATED_AT')} title="Latest Repositories" />
+            </View>
+      </Menu>
+    </View>
+  );
+};
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
@@ -19,25 +91,30 @@ const RepositoryList = () => {
     navigate(id);
   }
   const navigate = useNavigate();
+  const [orderDirection, setOrderDirection] = useState('ASC');
+  // ASC DESC
+  const [orderBy, setOrderBy] = useState('CREATED_AT');
 
-  const { repositories } = useRepositories();
-
+  const { repositories } = useRepositories({orderBy, orderDirection});
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
 
   return (
+    <PaperProvider>
       <FlatList
         data={repositoryNodes}
         ItemSeparatorComponent={ItemSeparator}
         renderItem={({ item }) => (
-          <View style={{ marginBottom: 15 }}>
+          <View style={{ marginBottom: 15, flex:1 }}>
             <TouchableOpacity onPress={() => handleViewChange(item.id)}>
               <RepositoryItems props={item}/>
             </TouchableOpacity>
           </View>
           )}
-        keyExtractor={(item) => item.id}/>
+          ListHeaderComponent={() =>   <HeaderFilter orderBy={orderBy} orderDirection={orderDirection} setOrderBy={setOrderBy} setOrderDirection={setOrderDirection} />}
+          keyExtractor={(item) => item.id}/>
+    </PaperProvider>
   );
 };
 
--- Fullstackopen/Osa_10/rate-repository-app/src/hooks/useRepositories.js
+++ Fullstackopen/Osa_10/rate-repository-app/src/hooks/useRepositories.js
@@ -1,9 +1,14 @@
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useQuery } from '@apollo/client';
-const useRepositories = () => {
+const useRepositories = (props) => {
+  console.log(props)
  const {data, error, loading} = useQuery(GET_REPOSITORIES, {
-  fetchPolicy: 'cache-and-network'
+  fetchPolicy: 'cache-and-network',
+  variables: {
+    "orderBy": props.orderBy,
+    "orderDirection": props.orderDirection
+  },
 });
   if (loading) {
     return { loading: true, error: null, repositories: null };
